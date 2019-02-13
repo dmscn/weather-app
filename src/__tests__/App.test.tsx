559c8d847fde52c3e9998f36492449284db9541f
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-02-13 11:10:58 -0200

    Isolated App tests at __tests__ and upgraded to use Enzyme and snapshot

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import App from '../App';
+
+import { shallow } from 'enzyme';
+
+it('renders without crashing', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toMatchSnapshot();
+});
