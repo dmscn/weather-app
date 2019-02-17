8613ef6782ae6eb47ec7ce22237e57fe49f95b4b
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-02-17 17:44:23 -0300

    Implemented API fetch call

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import LoadingScreen from './Components/LoadingScreen';
 import SearchScreen from './Components/SearchScreen';
+import ResultScreen from './Components/ResultScreen';
+
+const WEATHER_API_BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+const APPID = "638a471aca7521703a926ad9c4c08eec"
 
 export interface State {
   location?: string,
   loading: boolean,
+  weatherData?: any,
 }
 export default class App extends Component<any, State> {
   state = {
     location: undefined,
     loading: false,
+    weatherData: undefined,
   }
 
   locationSubmit = async (location: string): Promise<any> => {
     await this.setState({location, loading: true});
-    let result = await fetch(`${process.env.WEATHER_API_BASE_URL}?q=${location}`);
-    result = await result.json();
-    console.log(result);
+    const result = (await axios.get(`${WEATHER_API_BASE_URL}?q=${location}&APPID=${APPID}`)).data;
+    this.setState({ weatherData: result });
   }
   
   render(): JSX.Element {
-    const { location, loading } = this.state;
+    const { location, loading, weatherData } = this.state;
 
+    if(weatherData) return <ResultScreen data={weatherData} /> 
     if(loading && location) return <LoadingScreen location={location}/>;
     return <SearchScreen onSubmit={this.locationSubmit}/>;
   }
