36eb871774ede6e0114474ec2b4954c5fed76699
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-02-19 18:00:01 -0300

    Finished OtherInfo component

--- src/Components/ResultScreen/OtherInfo/OtherInfo.tsx
+++ src/Components/ResultScreen/OtherInfo/OtherInfo.tsx
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
+import moment from 'moment';
 
 import styles from './OtherInfo.module.css';
 
-export default class OtherInfo extends Component {
-  render() {
+export interface Props {
+  humidity: number,
+  sunrise: number,
+  sunset: number,
+}
+
+export default class OtherInfo extends Component<Props> {
+
+  formatTime = (time: number): string => {
+    return moment(new Date(time * 1000)).format('LT');
+  }
+  
+  render(): JSX.Element {
+    const { humidity, sunrise, sunset } = this.props;
+
     return (
       <section className={styles.Container}>
-        OtherInfo
+        <h4>Umidade: {humidity}%</h4>
+        <h4>Nascer do Sol: {this.formatTime(sunrise)}</h4>
+        <h4>Pôr do Sol: {this.formatTime(sunset)}</h4>
       </section>
     )
   }
--- src/Components/ResultScreen/ResultScreen.tsx
+++ src/Components/ResultScreen/ResultScreen.tsx
@@ -4,6 +4,7 @@ import moment from 'moment';
 import styles from './ResultScreen.module.css';
 import HeaderInfo from './HeaderInfo';
 import MainInfo from './MainInfo';
+import OtherInfo from './OtherInfo';
 
 moment.locale('pt-BR');
 export interface Props {
@@ -23,6 +24,8 @@ export default class ResultScreen extends Component<Props> {
           tempMax={data.main.temp_max}
           tempMin={data.main.temp_min}
         />
+        <hr />
+        <OtherInfo humidity={data.main.humidity} sunrise={data.sys.sunrise} sunset={data.sys.sunset} />
       </main>
     )
   }
